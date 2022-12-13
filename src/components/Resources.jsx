d095fcdeff43e4cc0d2d048cbeba5cf28adebd0b
Tyler Smith <Tysocialhq@gmail.com>
2022-12-13 17:50:04 -0600

    updating Resources

--- src/components/Resources.jsx
+++ src/components/Resources.jsx
@@ -19,36 +19,36 @@ const Resources = () => {
           <img src={block} loading='lazy'  className="w-48 h-48 
           lg:w-64 mx-auto md:mx-0 lg:h-64 xl:w-[290px] xl:h-[250px]  xl:mr-4" alt="block graphic" />
           <div className='flex flex-col items-start justify-center mt-4 text-left'>
-          <h1 className='text-4xl font-normal leading-10 tracking-wide '>Title</h1>
+          <h1 className='text-4xl font-normal leading-10 tracking-wide '>Learn</h1>
           <p className='mt-6 leading-5 tracking-wide w-[300px] lg:w-[240px] xl:w-[380px] h-[90px] '>Vero repellat molestiae fugit sunt enim eos. Qui ipsa officiis optio non ducimus sed sunt architecto ut. Hic explicabo ratione quaerat eaque similique commodi excepturi.</p>
-          <button className="bg-[#00A3FF] w-[220px] h-[50px] rounded-lg text-white mt-8 lg:mt-20 xl:mt-4">Whitepaper</button>
+          <button className="bg-[#00A3FF] w-[220px] h-[50px] rounded-lg text-white mt-8 lg:mt-20 xl:mt-4">Learn More</button>
         </div>
         </div>
         <div className=' w-[350px] xl:w-[520px] mt-16 lg:mt-24 flex flex-col lg:flex-row ml-16 lg:ml-0 '>
           <img src={wallet} className="w-48 h-48 
           lg:w-64 lg:h-64 xl:w-[290px] xl:h-[250px] mx-auto md:mx-0  xl:mr-4" alt="wallet graphic" />
           <div className='flex flex-col items-start justify-center mt-4 text-left'>
-          <h1 className='text-4xl font-normal leading-10 tracking-wide '>Title</h1>
+          <h1 className='text-4xl font-normal leading-10 tracking-wide '>Taxes</h1>
           <p className='mt-6 leading-5 tracking-wide w-[300px] lg:w-[240px] xl:w-[380px] h-[90px] '>Vero repellat molestiae fugit sunt enim eos. Qui ipsa officiis optio non ducimus sed sunt architecto ut. Hic explicabo ratione quaerat eaque similique commodi excepturi.</p>
-          <button className="bg-[#00A3FF] w-[220px] h-[50px] rounded-lg text-white mt-8 lg:mt-20 xl:mt-4">Whitepaper</button>
+          <button className="bg-[#00A3FF] w-[220px] h-[50px] rounded-lg text-white mt-8 lg:mt-20 xl:mt-4">Learn More</button>
         </div>
         </div>
         <div className=' w-[350px] lg:w-[520px]  mt-16 lg:mt-24 flex flex-col lg:flex-row ml-16 lg:ml-0'>
           <img src={trade} loading='lazy'  className="w-48 h-48 mx-auto md:mx-0
           lg:w-64 lg:h-64 xl:w-[290px] xl:h-[250px]  xl:mr-4" alt="exchange graphic" />
           <div className='flex flex-col items-start justify-center mt-4 text-left'>
-          <h1 className='text-4xl font-normal leading-10 tracking-wide '>Title</h1>
+          <h1 className='text-4xl font-normal leading-10 tracking-wide '>Docs</h1>
           <p className='mt-6 leading-5 tracking-wide w-[300px] lg:w-[240px] xl:w-[380px] h-[90px] '>Vero repellat molestiae fugit sunt enim eos. Qui ipsa officiis optio non ducimus sed sunt architecto ut. Hic explicabo ratione quaerat eaque similique commodi excepturi.</p>
-          <button className="bg-[#00A3FF] w-[220px] h-[50px] rounded-lg text-white mt-8 lg:mt-20 xl:mt-4">Whitepaper</button>
+          <button className="bg-[#00A3FF] w-[220px] h-[50px] rounded-lg text-white mt-8 lg:mt-20 xl:mt-4">View Docs</button>
         </div>
         </div>
         <div className=' w-[350px] lg:w-[500px] mt-16 lg:mt-24 flex flex-col lg:flex-row ml-16 lg:ml-0'>
           <img src={lock} className="w-48 h-48 
           lg:w-60 mx-auto lg:h-60 xl:w-[290px] xl:h-[250px] md:mx-0 " loading='lazy'  alt="lock graphic" />
           <div className='flex flex-col items-start justify-center mt-4 text-left'>
-          <h1 className='text-4xl font-normal leading-10 tracking-wide '>Title</h1>
+          <h1 className='text-4xl font-normal leading-10 tracking-wide '>Investors</h1>
           <p className='mt-6 leading-5 tracking-wide w-[300px] lg:w-[240px] xl:w-[380px] h-[90px] '>Vero repellat molestiae fugit sunt enim eos. Qui ipsa officiis optio non ducimus sed sunt architecto ut. Hic explicabo ratione quaerat eaque similique commodi excepturi.</p>
-          <button className="bg-[#00A3FF] w-[220px] h-[50px] rounded-lg text-white mt-8 lg:mt-20 xl:mt-4">Whitepaper</button>
+          <button className="bg-[#00A3FF] w-[220px] h-[50px] rounded-lg text-white mt-8 lg:mt-20 xl:mt-4">View Report</button>
         </div>
         </div>
       </div>
